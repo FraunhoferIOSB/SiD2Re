bde5c64070d014e2e7b96e36205d0a97b2215b07
Benedikt Stratmann <benedikt.stratmann@iosb.fraunhofer.de>
2025-12-18 13:07:38 +0100

    feature: add explizit Reoocuring Drift Behaviour

--- src/sid2re/driftgenerator/concept/drift_behaviours/__init__.py
+++ src/sid2re/driftgenerator/concept/drift_behaviours/__init__.py
@@ -1,10 +1,32 @@
-from sid2re.driftgenerator.concept.drift_behaviours.base_drift_behaviour import BaseDriftBehaviour
-from sid2re.driftgenerator.concept.drift_behaviours.faulty_sensor_drift_behaviour import FaultySensorDriftBehaviour
-from sid2re.driftgenerator.concept.drift_behaviours.gradual_drift_behaviour import GradualDriftBehaviour
-from sid2re.driftgenerator.concept.drift_behaviours.incremental_drift_behaviour import IncrementalDriftBehaviour
-from sid2re.driftgenerator.concept.drift_behaviours.sudden_drift_behaviour import SuddenDriftBehaviour
+from sid2re.driftgenerator.concept.drift_behaviours.base_drift_behaviour import (
+    BaseDriftBehaviour,
+)
+from sid2re.driftgenerator.concept.drift_behaviours.faulty_sensor_drift_behaviour import (
+    FaultySensorDriftBehaviour,
+    ReoccuringFaultySensorDriftBehaviour,
+)
+from sid2re.driftgenerator.concept.drift_behaviours.gradual_drift_behaviour import (
+    GradualDriftBehaviour,
+    ReoccuringGradualDriftBehaviour,
+)
+from sid2re.driftgenerator.concept.drift_behaviours.incremental_drift_behaviour import (
+    IncrementalDriftBehaviour,
+    ReoccuringIncrementalDriftBehaviour,
+)
+from sid2re.driftgenerator.concept.drift_behaviours.sudden_drift_behaviour import (
+    SuddenDriftBehaviour,
+    ReoccuringSuddenDriftBehaviour,
+)
+
 
 __all__ = [
-    'BaseDriftBehaviour', 'FaultySensorDriftBehaviour', 'GradualDriftBehaviour', 'IncrementalDriftBehaviour',
+    'BaseDriftBehaviour',
+    'FaultySensorDriftBehaviour',
+    'GradualDriftBehaviour',
+    'IncrementalDriftBehaviour',
     'SuddenDriftBehaviour',
+    'ReoccuringFaultySensorDriftBehaviour',
+    'ReoccuringGradualDriftBehaviour',
+    'ReoccuringIncrementalDriftBehaviour',
+    'ReoccuringSuddenDriftBehaviour',
 ]
--- src/sid2re/driftgenerator/concept/drift_behaviours/incremental_drift_behaviour.py
+++ src/sid2re/driftgenerator/concept/drift_behaviours/incremental_drift_behaviour.py
@@ -1,5 +1,9 @@
-from sid2re.driftgenerator.concept.drift_behaviours.base_drift_behaviour import BaseDriftBehaviour
-from sid2re.driftgenerator.concept.drift_transition_functions import BaseTransitionFunction
+from sid2re.driftgenerator.concept.drift_behaviours.base_drift_behaviour import (
+    BaseDriftBehaviour,
+)
+from sid2re.driftgenerator.concept.drift_transition_functions import (
+    BaseTransitionFunction,
+)
 from sid2re.driftgenerator.utils.type_aliases import NumberArray
 
 
@@ -9,6 +13,34 @@ class IncrementalDriftBehaviour(BaseDriftBehaviour):
     An incremental drift represents a continuous transfer from one concept to another. The coefficients are therefore
     interpolated between the start and the end state.
     """
+
+    def __init__(
+        self,
+        drift_time: float,
+        drift_radius: float,
+        coefficient_shift: NumberArray,
+        reoccurring: bool = False,
+    ) -> None:
+        """
+        Initialize the drift behavior.
+
+        Parameters
+        ----------
+        drift_time : float
+            The time at which the drift occurs.
+        drift_radius : float
+            The radius around the drift time within which the drift effect is active.
+        coefficient_shift : NumberArray
+            The amount by which coefficients are shifted during the drift.
+        reoccurring : bool
+            Indicates if the drift occurs repeatedly.
+        """
+        super().__init__(
+            drift_time=drift_time,
+            drift_radius=drift_radius,
+            coefficient_shift=coefficient_shift,
+            reoccurring=reoccurring,
+        )
 
     def _compute_coeff_delta(
         self,
@@ -33,10 +65,14 @@ class IncrementalDriftBehaviour(BaseDriftBehaviour):
         NumberArray
             The computed change in coefficients.
         """
-        return transition_behaviour.transition_coefficient(
-            current_time, self.drift_time,
-            self.drift_radius,
-        ) * self.coefficient_shift
+        return (
+            transition_behaviour.transition_coefficient(
+                current_time,
+                self.drift_time,
+                self.drift_radius,
+            )
+            * self.coefficient_shift
+        )
 
     @property
     def _drift_name(self) -> str:
@@ -49,3 +85,41 @@ class IncrementalDriftBehaviour(BaseDriftBehaviour):
             The name of the drift behavior: 'incremental'.
         """
         return 'incremental'
+
+
+class ReoccuringIncrementalDriftBehaviour(IncrementalDriftBehaviour):
+    """Specify the transformation of the concept coefficients caused by an incremental drift.
+
+    An incremental drift represents a continuous transfer from one concept to another. The coefficients are therefore
+    interpolated between the start and the end state.
+
+    This drift is reoccuring, so it will go trough a phase transition A -> B -> A.
+    """
+
+    def __init__(
+        self,
+        drift_time: float,
+        drift_radius: float,
+        coefficient_shift: NumberArray,
+        reoccurring: bool = True,
+    ) -> None:
+        """
+        Initialize the drift behavior.
+
+        Parameters
+        ----------
+        drift_time : float
+            The time at which the drift occurs.
+        drift_radius : float
+            The radius around the drift time within which the drift effect is active.
+        coefficient_shift : NumberArray
+            The amount by which coefficients are shifted during the drift.
+        reoccurring : bool
+            Indicates if the drift occurs repeatedly.
+        """
+        super().__init__(
+            drift_time=drift_time,
+            drift_radius=drift_radius,
+            coefficient_shift=coefficient_shift,
+            reoccurring=reoccurring,
+        )
